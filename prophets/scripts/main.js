2785681db8685e47e04ce91b5a924a1d4c96a338
Tristan Galloway <tristan.a.galloway@gmail.com>
2024-11-14 10:24:09 -0700

    Finished base prep assignments

---
+++ prophets/scripts/main.js
@@ -0,0 +1,39 @@
+const url = 'https://brotherblazzard.github.io/canvas-content/latter-day-prophets.json';
+
+const cards = document.querySelector('#cards');
+
+async function getProphetData() {
+    const response = await fetch(url)
+    const data = await response.json();
+    // console.table(data.prophets);
+    displayProphets(data.prophets);
+}
+
+const displayProphets = (prophets) => {
+    prophets.forEach((prophet) => {
+        const card = document.createElement('section');
+        const fullName = document.createElement('h2');
+        const portrait = document.createElement('img');
+        const birthdate = document.createElement('p');
+        const birthplace = document.createElement('p');
+
+        fullName.textContent = `${prophet.name} ${prophet.lastname}`;
+        birthdate.textContent = `Date of Birth: ${prophet.birthdate}`;
+        birthplace.textContent = `Place of Birth: ${prophet.birthplace}`;
+
+        portrait.setAttribute('src', prophet.imageurl);
+        portrait.setAttribute('alt', `Portrait of ${prophet.name} ${prophet.lastname}`); // fill in the blank
+        portrait.setAttribute('loading', 'lazy');
+        portrait.setAttribute('width', '340');
+        portrait.setAttribute('height', '440');
+
+        card.appendChild(fullName);
+        card.appendChild(birthdate);
+        card.appendChild(birthplace);
+        card.appendChild(portrait);
+
+        cards.appendChild(card);
+    });
+}
+
+getProphetData();
