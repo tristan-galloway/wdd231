fe71cc209981f4924a630d391a5c0849bd4cf28c
Tristan Galloway <tristan.a.galloway@gmail.com>
2024-11-20 20:25:28 -0700

    Finished weather api assignment

---
+++ weather/scripts/weather.js
@@ -0,0 +1,36 @@
+// select HTML elements in the document
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+
+const lat = "49.75";
+const lon = "6.64";
+const key = "496e909bd83237f140fdea7c6ff76865";
+
+const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=imperial&appid=${key}`;
+
+async function apiFetch() {
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+            const data = await response.json();
+            console.log(data); // testing only
+            displayResults(data);
+        } else {
+            throw Error(await response.text());
+        }
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+apiFetch();
+
+function displayResults(data) {
+    currentTemp.innerHTML = `${data.main.temp}&deg;F`;
+    const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+    let desc = data.weather[0].description;
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('alt', data.weather[0].main);
+    captionDesc.textContent = `${desc}`;
+};
